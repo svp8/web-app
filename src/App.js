b2a3988123f9a504d2159c6a0dc276eab7d8c965
svp8 <azaz08082001@gmail.com>
2021-08-01 18:13:52 +0500

    верстка

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header>
+        <div className="headerLeft">
+          <img src="img/logo.png" width="40" height="40" alt="ss" />
+          <div className="headerInfo">
+            <h3 style={{ "text-transform": "uppercase" }}>React sneakers</h3>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+        <ul className="headerRight">
+          <li style={{ "margin-right": "30px" }}>
+            <img src="img/cart.svg" alt="ss" />
+            <span>1250 руб</span>
+          </li>
+          <li>
+            <img src="img/user.svg" alt="ss" />
+          </li>
+        </ul>
       </header>
+      <div className="content">
+        <h1 style={{ "margin-bottom": "40px" }}>Все кроссовки</h1>
+        <div style={{ display: "flex" ,}}>
+          <div className="card">
+            <img src="img/sneakers/1.jpg" width="133" height="112" alt="ss" />
+            <h5>Мужские кроссовки Nike</h5>
+            <div className="cardBottom">
+              <div style={{ display: "flex", "flex-direction": "column" }}>
+                <span>Цена:</span>
+                <b>12 999 руб</b>
+              </div>
+              <button className="button">
+                <img src="img/plus.svg" width="11" height="11" alt="ss" />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img src="img/sneakers/2.jpg" width="133" height="112" alt="ss" />
+            <h5>Мужские кроссовки Nike</h5>
+            <div className="cardBottom">
+              <div style={{ display: "flex", "flex-direction": "column" }}>
+                <span>Цена:</span>
+                <b>12 999 руб</b>
+              </div>
+              <button className="button">
+                <img src="img/plus.svg" width="11" height="11" alt="ss" />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img src="img/sneakers/3.jpg" width="133" height="112" alt="ss" />
+            <h5>Мужские кроссовки Nike</h5>
+            <div className="cardBottom">
+              <div style={{ display: "flex", "flex-direction": "column" }}>
+                <span>Цена:</span>
+                <b>12 999 руб</b>
+              </div>
+              <button className="button">
+                <img src="img/plus.svg" width="11" height="11" alt="ss" />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img src="img/sneakers/4.jpg" width="133" height="112" alt="ss" />
+            <h5>Мужские кроссовки Nike</h5>
+            <div className="cardBottom">
+              <div style={{ display: "flex", "flex-direction": "column" }}>
+                <span>Цена:</span>
+                <b>12 999 руб</b>
+              </div>
+              <button className="button">
+                <img src="img/plus.svg" width="11" height="11" alt="ss" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
